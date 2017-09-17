55817b9d78c35f3fcf8bee8d89078e22e4c6bbde
Mike <mike@MacBook.local>
2017-09-17 19:41:35 +0800

    commit the project

---
+++ Soil/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Soil
+//
+//  Created by Mike on 17/09/2017.
+//  Copyright © 2017 Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Soil/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Soil
+//
+//  Created by Mike on 17/09/2017.
+//  Copyright © 2017 Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
