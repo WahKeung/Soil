721a38bd5f96f2fde0d0dc9f8b23046cd4c271cd
Mike <mike.leong.kosun@gmail.com>
2017-09-24 19:40:18 +0800

    Xcode recommand setting and MoreTableViewController function

--- Soil/MoreTableViewController.m
+++ Soil/MoreTableViewController.m
@@ -11,6 +11,7 @@
 #import "AppDelegate.h"
 #import "KeywordEntity+CoreDataClass.h"
 #import <MessageUI/MessageUI.h>
+#import <StoreKit/StoreKit.h>
 
 @interface MoreTableViewController ()<MFMailComposeViewControllerDelegate>
 @property (weak, nonatomic) IBOutlet GADBannerView *bannerView;
@@ -45,11 +46,20 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     if (indexPath.section==0 && indexPath.row==0) {
         [self deleteAllHistory];
+        [self showAlertControllerWithMessage:@"已经清除搜索历史"];
         [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
     } else if (indexPath.section==1) {
         if ([MFMailComposeViewController canSendMail]) {
             [self sendMail];
         } else {
+            [tableView deselectRowAtIndexPath:indexPath animated:YES];
+        }
+    } else if (indexPath.section==2) {
+        if (indexPath.row==2) {
+            [self jumpToAppStore];
+            [tableView deselectRowAtIndexPath:indexPath animated:YES];
+        } else if (indexPath.row==3) {
+            [self showActivityController];
             [tableView deselectRowAtIndexPath:indexPath animated:YES];
         }
     }
@@ -72,7 +82,7 @@
     // 设置邮件代理
     [mailCompose setMailComposeDelegate:self];
     // 设置收件人
-    [mailCompose setToRecipients:@[@"mike.leong.kosun@gmail.com"]];
+    [mailCompose setToRecipients:@[@"appformike@163.com"]];
     // 设置抄送人
 //    [mailCompose setCcRecipients:@[@"1622849369@qq.com"]];
     // 设置密送人
@@ -98,29 +108,74 @@
 
 #pragma mark - MFMailComposeViewControllerDelegate的代理方法：
 -(void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error{
+    
+    NSString *message = nil;
     switch (result) {
         case MFMailComposeResultCancelled:
             NSLog(@"Mail send canceled: 用户取消编辑");
             break;
         case MFMailComposeResultSaved:
             NSLog(@"Mail saved: 用户保存邮件");
+            message = @"已保存邮件";
             break;
         case MFMailComposeResultSent:
             NSLog(@"Mail sent: 用户点击发送");
+            message = @"已发送邮件";
             break;
         case MFMailComposeResultFailed:
             NSLog(@"Mail send errored: %@ : 用户尝试保存或发送邮件失败", [error localizedDescription]);
+            message = @"尝试保存或者发送邮件失败";
             break;
     }
     // 关闭邮件发送视图
     [self dismissViewControllerAnimated:YES completion:^{
-        if (result==MFMailComposeResultSent) {
-            UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"反馈作者" message:@"邮件已发送" preferredStyle:UIAlertControllerStyleAlert];
+        if (result==MFMailComposeResultSent || result==MFMailComposeResultSaved || result==MFMailComposeResultFailed) {
+            UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"反馈作者" message:message preferredStyle:UIAlertControllerStyleAlert];
             UIAlertAction *action = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleCancel handler:nil];
             [alertController addAction:action];
             [self presentViewController:alertController animated:YES completion:nil];
         }
     }];
 }
+
+#pragma mark -
+
+- (void)dismissAlertController:(UIAlertController *)alertController {
+    [alertController dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)showAlertControllerWithMessage:(NSString *)message {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"提示" message:message preferredStyle:UIAlertControllerStyleAlert];
+    [self presentViewController:alertController animated:YES completion:^{
+        [self performSelector:@selector(dismissAlertController:) withObject:alertController afterDelay:1];
+    }];
+}
+
+#pragma mark -
+
+- (void)jumpToAppStore {
+    UIApplication *app = [UIApplication sharedApplication];
+    NSString *appID = @"1287143610";
+    NSString *urlString = [NSString stringWithFormat:@"itms-apps://itunes.apple.com/app/id%@?action=write-review", appID];
+    NSURL *url = [NSURL URLWithString:urlString];
+    if ([app canOpenURL:url]) {
+        [app openURL:url];
+    } else {
+        [self showAlertControllerWithMessage:@"打开 App Store 失败"];
+    }
+
+}
+
+#pragma mark -
+
+- (void)showActivityController {
+    NSString *appID = @"1287143610";
+    NSString *urlString = [NSString stringWithFormat:@"itms-apps://itunes.apple.com/app/id%@?action=write-review", appID];
+    NSArray *activityItems = @[urlString];
+    NSArray *applicationActivities = @[];
+    UIActivityViewController *activityController = [[UIActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:applicationActivities];
+    [self presentViewController:activityController animated:YES completion:nil];
+}
+
 
 @end
